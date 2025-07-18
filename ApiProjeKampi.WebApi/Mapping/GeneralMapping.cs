a4c4e890eb711257ca34a05bef5b5375ae200fb9
MuratYucedag <yucedagmurat23@gmail.com>
2025-07-18 09:49:57 +0300

    ...

--- ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -1,6 +1,7 @@
 ﻿using ApiProjeKampi.WebApi.Dtos.AboutDtos;
 using ApiProjeKampi.WebApi.Dtos.CategoryDtos;
 using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
+using ApiProjeKampi.WebApi.Dtos.ImageDtos;
 using ApiProjeKampi.WebApi.Dtos.MessageDtos;
 using ApiProjeKampi.WebApi.Dtos.NotificationDtos;
 using ApiProjeKampi.WebApi.Dtos.ProductDtos;
@@ -44,6 +45,11 @@ namespace ApiProjeKampi.WebApi.Mapping
             CreateMap<Reservation, CreateReservationDto>().ReverseMap();
             CreateMap<Reservation, UpdateReservationDto>().ReverseMap();
             CreateMap<Reservation, GetReservationByIdDto>().ReverseMap();
+
+            CreateMap<Image, ResultImageDto>().ReverseMap();
+            CreateMap<Image, CreateImageDto>().ReverseMap();
+            CreateMap<Image, UpdateImageDto>().ReverseMap();
+            CreateMap<Image, GetImageByIdDto>().ReverseMap();
         }
     }
 }
